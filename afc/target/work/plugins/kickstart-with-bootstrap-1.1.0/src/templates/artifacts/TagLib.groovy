5fb620562e603c2eef97540ca3ff9f04fc2af8fb
karangun <gun@pupasoft.com>
2014-05-09 16:45:29 +0700

    Merge branch 'master' of https://github.com/karangun/afc

--- afc/target/work/plugins/kickstart-with-bootstrap-1.1.0/src/templates/artifacts/TagLib.groovy
+++
@@ -1,8 +0,0 @@
-@artifact.package@/**
- * @artifact.name@
- * A taglib library provides a set of reusable tags to help rendering the views.
- */
-class @artifact.name@ {
-    static defaultEncodeAs = 'html'
-    //static encodeAsForTags = [tagName: 'raw']
-}
