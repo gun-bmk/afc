7953ea0d2b632719fdac93195116ba28d073c8d3
karangun <gun@pupasoft.com>
2014-05-09 14:09:46 +0700

    domain

--- afc/grails-app/domain/afc/Comment.groovy
+++ afc/grails-app/domain/afc/Comment.groovy
@@ -1,35 +1,21 @@
 package afc
 
-/**
- * Comment
- * A domain class describes the data object and it's mapping to the database
- */
+import org.joda.time.LocalDateTime
+
 class Comment {
 
-	/* Default (injected) attributes of GORM */
-//	Long	id
-//	Long	version
+	String comment
+	String authorName
+	LocalDateTime timeStamp
+	int like = 0
+	boolean topComment = false
 	
-	/* Automatic timestamping of GORM */
-//	Date	dateCreated
-//	Date	lastUpdated
-	
-//	static	belongsTo	= []	// tells GORM to cascade commands: e.g., delete this object if the "parent" is deleted.
-//	static	hasOne		= []	// tells GORM to associate another domain object as an owner in a 1-1 mapping
-//	static	hasMany		= []	// tells GORM to associate other domain objects for a 1-n or n-m mapping
-//	static	mappedBy	= []	// specifies which property should be used in a mapping 
+	static belongsTo = [match :Match]
 	
     static	mapping = {
     }
     
 	static	constraints = {
+		comment blank: false
     }
-	
-	/*
-	 * Methods of the Domain Class
-	 */
-//	@Override	// Override toString for a nicer / more descriptive UI 
-//	public String toString() {
-//		return "${name}";
-//	}
 }
--- afc/grails-app/domain/afc/Match.groovy
+++ afc/grails-app/domain/afc/Match.groovy
@@ -1,23 +1,15 @@
 package afc
 
-/**
- * Match
- * A domain class describes the data object and it's mapping to the database
- */
-class Match {
+import org.joda.time.*
 
-	/* Default (injected) attributes of GORM */
-//	Long	id
-//	Long	version
+class Match {
 	
-	/* Automatic timestamping of GORM */
-//	Date	dateCreated
-//	Date	lastUpdated
+	LocalDateTime date
+	String place
 	
-//	static	belongsTo	= []	// tells GORM to cascade commands: e.g., delete this object if the "parent" is deleted.
-//	static	hasOne		= []	// tells GORM to associate another domain object as an owner in a 1-1 mapping
-//	static	hasMany		= []	// tells GORM to associate other domain objects for a 1-n or n-m mapping
-//	static	mappedBy	= []	// specifies which property should be used in a mapping 
+	static belongsTo = [home: Team, away: Team]
+	
+	static hasMany = [comments: Comment, commentators: Commentator]
 	
     static	mapping = {
     }
@@ -25,11 +17,4 @@ class Match {
 	static	constraints = {
     }
 	
-	/*
-	 * Methods of the Domain Class
-	 */
-//	@Override	// Override toString for a nicer / more descriptive UI 
-//	public String toString() {
-//		return "${name}";
-//	}
 }
