be2ff13e79822e34a3f172d14749cfb2ec826df5
karangun <gun@pupasoft.com>
2014-05-10 16:16:52 +0700

    add image 2

---
+++ afc/test/unit/afc/BackendControllerSpec.groovy
@@ -0,0 +1,20 @@
+package afc
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(BackendController)
+class BackendControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
